33ce41c37424ec9bde1deca69c8fdebf8f0cb5f3
eecheverry <50468250+eecheverry@users.noreply.github.com>
2019-05-09 19:36:14 -0500

    Add files via upload
    Buenas noches profesor, 
    
    Taller clase 09 de mayo.
    
    Integrantes:
    
    Laura Carolina Cardozo Jimenez
    Jorge Ivan Bustamante
    Luis Enrique Echeverry Davila

---
+++ contactenos.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contactenos</title>
+	<style type="text/css">
+    body{
+        background-color: CYAN ;
+    }
+    </style>
+</head>
+<body>
+<img src="hd_contactenos.jpg" hspace=00 , width=1330 ,  HEIGHT=240  />
+ <form action="mailto:chinhondas2014@outlook.com" method="post" enctype="text/plain">
+<center><table bgcolor="#cccccc" border="0" cellpadding="6"
+cellspacing="0" width="400">
+<H3>Contactenos</H3>
+<tr>
+<td align="right" valign="top"><b>Nombre</b></td>
+<td><input type="text" size="25" name="Nombre"> </td>
+</tr>
+<tr>
+<td align="right" valign="top"><b>E-mail</b></td>
+<td><input type="text" size="25" name="e-mail"> </td>
+</tr>
+<td align="right" valign="top"><b>Telefono</b></td>
+<td><input type="text" size="25" name="Telefono"> </td>
+</tr>
+<tr>
+<td align="right" valign="top"><b>Direccion</b></td>
+<td><input type="text" size="25" name="Direccion"> </td>
+</tr>
+<tr>
+<td align="right" valign="top"><b>Barrio</b></td>
+<td><input type="text" size="25" name="Barrio"> </td>
+</tr>
+<tr>
+<td align="right" valign="top"><b>Municipio</b></td>
+<td><input type="text" size="25" name="Municipio"> </td>
+</tr>
+<tr>
+<td align="right" valign="top"><b>Pulsa una vez</b>&nbsp; <b>&gt;</b> </td>
+<td align="center">&nbsp; <input
+type="submit" name="Enviar datos" value=" Enviar "><input type="reset" value="Borrar" name="B1"> </td>
+</tr>
+</table>
+ <h3>Tame - Arauca Sede Principal Cll 14 # 15 -20 </h3>
+ <h4>Correos chinhondas2014@outlook.com  - Bancomunicaciones@contactenos.com.co </h4>
+ <h5>Tel 3223232321 - 8884567 Horario De Atencion 8 Am a 12 Pm - 2 Pm a 5 Pm</h5>
+</center>
+</form>
+</body>
+</html>
